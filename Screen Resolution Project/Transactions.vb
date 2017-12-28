9df223a73916ed0986bfb55b0440039c36f7a1d3
smartgaastech <smartgaastech@gmail.com>
2017-12-28 23:00:56 +0400

    Fixed  Sales Return Issue
    Fixed sales return issue. Adjusted TED table with head , item and head
    sys id.

--- Screen Resolution Project/Transactions.vb
+++ Screen Resolution Project/Transactions.vb
@@ -6556,7 +6556,8 @@ Public Class Transactions
                                 'Added this code to implement tax in POS  - Ticket-https://evisionsoft.freshdesk.com/helpdesk/tickets/40 
                                 Dim taxCode As String = taxImpl.getLocationTaxCodeForItem(Location_Code, itemCodeForTaxEntry)
                                 Dim taxPercentage As Double = taxImpl.getTaxPercentageofItem(itemCodeForTaxEntry, Location_Code, TXN_Code, taxCode)
-                                Dim taxValueOfItem As Double = Round(taxImpl.calculateTaxValueofItem(itemCodeForTaxEntry, itemPriceForTaxEntry, Location_Code, TXN_Code, taxCode, taxPercentage), 3)
+                                Dim unroundtaxValueOfItem As Double = taxImpl.calculateTaxValueofItem(itemCodeForTaxEntry, itemPriceForTaxEntry, Location_Code, TXN_Code, taxCode, taxPercentage)
+                                Dim taxValueOfItem As Double = Round(unroundtaxValueOfItem, 3)
                                 'taxValueOfItem = taxValueOfItem
 
 
@@ -6578,7 +6579,7 @@ Public Class Transactions
                                         TEDTAX_NUM = dsTED.Tables("Table").Rows.Item(0).Item(0).ToString
                                     End If
 
-                                    stQuery = "SELECT ITED_TED_RATE,ITED_TED_CODE from OT_INVOICE_ITEM_TED where ITED_I_SYS_ID=" & rowI.Item(0).ToString & " and ITED_H_SYS_ID=" & rowI.Item(1).ToString & " and ITED_TED_TYPE_NUM=" & TEDTAX_NUM
+                                    stQuery = "SELECT ITED_TED_RATE,ITED_TED_CODE,ITED_SYS_ID from OT_INVOICE_ITEM_TED where ITED_I_SYS_ID=" & rowI.Item(0).ToString & " and ITED_H_SYS_ID=" & rowI.Item(1).ToString & " and ITED_TED_TYPE_NUM=" & TEDTAX_NUM
                                     dsTED = db.SelectFromTableODBC(stQuery)
                                     Dim TEDRATE As String = "0"
                                     Dim TEDCODE As String = ""
@@ -6590,8 +6591,8 @@ Public Class Transactions
                                         End If
 
                                         TEDCODE = dsTED.Tables("Table").Rows.Item(0).Item(1).ToString
-                                        stQuery = "INSERT INTO OT_CUST_SALE_RET_ITEM_TED (ITED_SYS_ID,ITED_H_SYS_ID,ITED_I_SYS_ID ,ITED_TED_CODE,ITED_TED_TYPE_NUM,ITED_TED_HEAD_ITEM_NUM,ITED_TED_BASIS, ITED_TED_CURR_CODE,ITED_TXN_CURR_CODE,ITED_TED_RATE,ITED_TAXABLE_FC_AMT, ITED_TAXABLE_LC_AMT,ITED_FC_AMT,ITED_LC_AMT,ITED_NET_FC_AMT,ITED_NET_LC_AMT,ITED_CR_UID,ITED_CR_DT)VALUES("
-                                        stQuery = stQuery & "ITED_SYS_ID.NEXTVAL" & "," & maxSYS_ID & "," & maxItemSYSID & ",'" & TEDCODE & "'," & TEDTAX_NUM & ",'2','R','" + Currency_Code + "','" + Currency_Code + "'," & TEDRATE & "," & itemPriceForTaxEntry - taxValueOfItem & "," & itemPriceForTaxEntry - taxValueOfItem & "," & taxValueOfItem & "," & taxValueOfItem & "," & taxValueOfItem & "," & taxValueOfItem & ",'" + LogonUser + "',sysdate)"
+                                        stQuery = "INSERT INTO OT_CUST_SALE_RET_ITEM_TED (ITED_SYS_ID,ITED_H_SYS_ID,ITED_I_SYS_ID ,ITED_TED_CODE,ITED_TED_TYPE_NUM,ITED_TED_HEAD_ITEM_NUM,ITED_TED_BASIS, ITED_TED_CURR_CODE,ITED_TXN_CURR_CODE,ITED_TED_RATE,ITED_TAXABLE_FC_AMT, ITED_TAXABLE_LC_AMT,ITED_FC_AMT,ITED_LC_AMT,ITED_NET_FC_AMT,ITED_NET_LC_AMT,ITED_CR_UID,ITED_CR_DT, ITED_INVH_SYS_ID, ITED_INVI_SYS_ID, ITED_ITED_SYS_ID, ITED_TED_TYPE_CODE, ITED_TED_INCL_EXCL_FLAG, ITED_TXN_UOM, ITED_TXN_QTY, ITED_USER_QTY, ITED_SET_OFF_PERC, ITED_SET_OFF_FC_AMT, ITED_SET_OFF_LC_AMT, ITED_UNROUND_FC_AMT,ITED_UNROUND_LC_AMT, ITED_UPDATED_YN_NUM, ITED_POST_TYPE_NUM)VALUES("
+                                        stQuery = stQuery & "ITED_SYS_ID.NEXTVAL" & "," & maxSYS_ID & "," & maxItemSYSID & ",'" & TEDCODE & "'," & TEDTAX_NUM & ",'2','R','" + Currency_Code + "','" + Currency_Code + "'," & TEDRATE & "," & itemPriceForTaxEntry - taxValueOfItem & "," & itemPriceForTaxEntry - taxValueOfItem & "," & taxValueOfItem & "," & taxValueOfItem & "," & taxValueOfItem & "," & taxValueOfItem & ",'" + LogonUser + "',sysdate," & rowI.Item(1) & "," & rowI.Item(0) & "," & dsTED.Tables("Table").Rows.Item(0).Item(2).ToString & ",'TAX','E','" & rowI.Item(5).ToString & "'," & rowI.Item(3) & "," & ItmQtyFound(0).Text & ",0,0,0," & unroundtaxValueOfItem & "," & unroundtaxValueOfItem & ",2,1)"
                                         errLog.WriteToErrorLog("QUERY INSERT ITEM TED TAX", stQuery, "")
                                         command.CommandText = stQuery
                                         command.ExecuteNonQuery()
@@ -6606,7 +6607,7 @@ Public Class Transactions
                                         TEDDIS_NUM = dsTED.Tables("Table").Rows.Item(0).Item(0).ToString
                                     End If
 
-                                    stQuery = "SELECT ITED_TED_RATE,ITED_TED_CODE from OT_INVOICE_ITEM_TED where ITED_I_SYS_ID=" & rowI.Item(0).ToString & " and ITED_H_SYS_ID=" & rowI.Item(1).ToString & " and ITED_TED_TYPE_NUM=" & TEDDIS_NUM
+                                    stQuery = "SELECT ITED_TED_RATE,ITED_TED_CODE,ITED_SYS_ID from OT_INVOICE_ITEM_TED where ITED_I_SYS_ID=" & rowI.Item(0).ToString & " and ITED_H_SYS_ID=" & rowI.Item(1).ToString & " and ITED_TED_TYPE_NUM=" & TEDDIS_NUM
                                     dsTED = db.SelectFromTableODBC(stQuery)
                                     Dim TEDRATE As String = "0"
                                     Dim TEDCODE As String = ""
@@ -6618,8 +6619,8 @@ Public Class Transactions
                                         End If
 
                                         TEDCODE = dsTED.Tables("Table").Rows.Item(0).Item(1).ToString
-                                        stQuery = "INSERT INTO OT_CUST_SALE_RET_ITEM_TED (ITED_SYS_ID,ITED_H_SYS_ID,ITED_I_SYS_ID ,ITED_TED_CODE,ITED_TED_TYPE_NUM,ITED_TED_HEAD_ITEM_NUM,ITED_TED_BASIS, ITED_TED_CURR_CODE,ITED_TXN_CURR_CODE,ITED_TED_RATE,ITED_TAXABLE_FC_AMT, ITED_TAXABLE_LC_AMT,ITED_FC_AMT,ITED_LC_AMT,ITED_NET_FC_AMT,ITED_NET_LC_AMT,ITED_CR_UID,ITED_CR_DT)VALUES("
-                                        stQuery = stQuery & "ITED_SYS_ID.NEXTVAL" & "," & maxSYS_ID & "," & maxItemSYSID & ",'" & TEDCODE & "'," + TEDDIS_NUM + ",'2','R','" + Currency_Code + "','" + Currency_Code + "'," + "0" + "," + rowI.Item(6).ToString + "," + rowI.Item(6).ToString + "," + ItmDisamtFound(0).Text + "," + ItmDisamtFound(0).Text + "," + ItmDisamtFound(0).Text + "," + ItmDisamtFound(0).Text + ",'" + LogonUser + "',sysdate)"
+                                        stQuery = "INSERT INTO OT_CUST_SALE_RET_ITEM_TED (ITED_SYS_ID,ITED_H_SYS_ID,ITED_I_SYS_ID ,ITED_TED_CODE,ITED_TED_TYPE_NUM,ITED_TED_HEAD_ITEM_NUM,ITED_TED_BASIS, ITED_TED_CURR_CODE,ITED_TXN_CURR_CODE,ITED_TED_RATE,ITED_TAXABLE_FC_AMT, ITED_TAXABLE_LC_AMT,ITED_FC_AMT,ITED_LC_AMT,ITED_NET_FC_AMT,ITED_NET_LC_AMT,ITED_CR_UID,ITED_CR_DT, ITED_INVH_SYS_ID, ITED_INVI_SYS_ID, ITED_ITED_SYS_ID, ITED_TED_TYPE_CODE, ITED_TED_INCL_EXCL_FLAG, ITED_TXN_UOM, ITED_TXN_QTY, ITED_USER_QTY, ITED_SET_OFF_PERC, ITED_SET_OFF_FC_AMT, ITED_SET_OFF_LC_AMT, ITED_UNROUND_FC_AMT,ITED_UNROUND_LC_AMT, ITED_UPDATED_YN_NUM, ITED_POST_TYPE_NUM)VALUES("
+                                        stQuery = stQuery & "ITED_SYS_ID.NEXTVAL" & "," & maxSYS_ID & "," & maxItemSYSID & ",'" & TEDCODE & "'," + TEDDIS_NUM + ",'2','R','" + Currency_Code + "','" + Currency_Code + "'," + "0" + "," + rowI.Item(6).ToString + "," + rowI.Item(6).ToString + "," + ItmDisamtFound(0).Text + "," + ItmDisamtFound(0).Text + "," + ItmDisamtFound(0).Text + "," + ItmDisamtFound(0).Text + ",'" + LogonUser + "',sysdate," & rowI.Item(1) & "," & rowI.Item(0) & "," & dsTED.Tables("Table").Rows.Item(0).Item(2).ToString & ",'TEDDIS','E','" & rowI.Item(5).ToString & "'," & rowI.Item(3) & "," & ItmQtyFound(0).Text & ",0,0,0," & ItmDisamtFound(0).Text & "," & ItmDisamtFound(0).Text & ",2,1)"
                                         errLog.WriteToErrorLog("QUERY INSERT ITEM TEDDIS", stQuery, "")
                                         'MAILLOGROWNUM = MAILLOGROWNUM + 1
                                         'MAILLOGValues.Add(MAILLOGROWNUM, "QUERY INSERT ITEM TEDDIS  -  " & stQuery)
